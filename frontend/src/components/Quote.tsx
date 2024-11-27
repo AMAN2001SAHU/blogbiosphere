8ae33fa4b2b06f0a8ef92845f6504a0df27240ab
Aman Sahu <91371067+AMAN2001SAHU@users.noreply.github.com>
2024-11-27 12:38:20 +0530

    Updated the Quote in Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -3,12 +3,13 @@ export const Quote = () => {
     <div className="bg-gray-100 h-screen flex justify-center flex-col items-center">
       <div className="flex justify-center flex-col max-w-lg ">
         <div className="text-3xl font-bold text-left">
-          "The custome support I received was exceptional. The support team went
-          above and beyond to address my concerns"
+          "Where creativity meets logic, and ideas transform into innovation—exploring the art of development, 
+          one project, one line of code, 
+          and one breakthrough at a time."
         </div>
         <div className="text-xl font-bold text-left mt-4">Julies Winfield</div>
         <div className="text-base font-light text-left text-slate-400">
-          CEO | Acme corp
+          Senior Dev
         </div>
       </div>
     </div>
